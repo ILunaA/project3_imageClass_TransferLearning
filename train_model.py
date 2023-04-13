c103021dc8930adc28891a38e20eb616f9bcb8fd
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-04-13 11:45:50 +0200

    issues with hyperparameter tuning
    Done download and s3 data upload

---
+++ train_model.py
@@ -0,0 +1,216 @@
+#TODO: Import your dependencies.
+#For instance, below are some dependencies you might need if you are using Pytorch
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.models as models
+import torchvision.transforms as transforms
+
+import argparse
+
+
+#TODO: Import dependencies for Debugging andd Profiling
+
+def test(model, test_loader,criterion, device):
+    '''
+    TODO: Complete this function that can take a model and a 
+          testing data loader and will get the test accuray/loss of the model
+          Remember to include any debugging/profiling hooks that you might need
+    '''
+    print("Testing Model on Whole Testing Dataset")
+    model.eval()
+    running_loss=0
+    running_corrects=0
+    
+    for inputs, labels in test_loader:
+        inputs=inputs.to(device)
+        labels=labels.to(device)
+        outputs=model(inputs)
+        loss=criterion(outputs, labels)
+        _, preds = torch.max(outputs, 1)
+        running_loss += loss.item() * inputs.size(0)
+        running_corrects += torch.sum(preds == labels.data).item()
+
+    total_loss = running_loss / len(test_loader.dataset)
+    total_acc = running_corrects/ len(test_loader.dataset)
+    print(f"Testing Accuracy: {100*total_acc}, Testing Loss: {total_loss}")
+
+def train(model, train_loader, criterion, optimizer, device):
+    '''
+    TODO: Complete this function that can take a model and
+          data loaders for training and will get train the model
+          Remember to include any debugging/profiling hooks that you might need
+    '''
+    epochs=25
+    best_loss=1e6
+    image_dataset={'train':train_loader, 'valid':validation_loader}
+    loss_counter=0
+    
+    for epoch in range(epochs):
+        for phase in ['train', 'valid']:
+            print(f"Epoch {epoch}, Phase {phase}")
+            if phase=='train':
+                model.train()
+            else:
+                model.eval()
+            running_loss = 0.0
+            running_corrects = 0
+            running_samples=0
+
+            for step, (inputs, labels) in enumerate(image_dataset[phase]):
+                inputs=inputs.to(device)
+                labels=labels.to(device)
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+
+                if phase=='train':
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                _, preds = torch.max(outputs, 1)
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data).item()
+                running_samples+=len(inputs)
+                if running_samples % 2000  == 0:
+                    accuracy = running_corrects/running_samples
+                    print("Images [{}/{} ({:.0f}%)] Loss: {:.2f} Accuracy: {}/{} ({:.2f}%) Time: {}".format(
+                            running_samples,
+                            len(image_dataset[phase].dataset),
+                            100.0 * (running_samples / len(image_dataset[phase].dataset)),
+                            loss.item(),
+                            running_corrects,
+                            running_samples,
+                            100.0*accuracy,
+                            time.asctime() # for measuring time for testing, remove for students and in the formatting
+                        )
+                    )
+                
+                #NOTE: Comment lines below to train and test on whole dataset
+                #if running_samples>(0.2*len(image_dataset[phase].dataset)):
+                #    break
+
+            epoch_loss = running_loss / running_samples
+            epoch_acc = running_corrects / running_samples
+            
+            if phase=='valid':
+                if epoch_loss<best_loss:
+                    best_loss=epoch_loss
+                else:
+                    loss_counter+=1
+
+        if loss_counter==1:
+            break
+    return model
+    
+def net():
+    '''
+    TODO: Complete this function that initializes your model
+          Remember to use a pretrained model
+    '''
+        model = models.resnet18(pretrained=True)
+
+        for param in model.parameters():
+            param.requires_grad = False   
+
+        num_features=model.fc.in_features
+        model.fc = nn.Sequential(
+                   nn.Linear(num_features, 133))
+        return model
+
+def create_data_loaders(data, batch_size):
+    '''
+    This is an optional function that you may or may not need to implement
+    depending on whether you need to use data loaders or not
+    '''
+    transform = transform.Compose([
+        transforms.RandomHorizontalFlip(p=0.5)
+        transforms.Resize((224,224)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,),(0.3081))
+    ])
+    
+    data = torchvision.datasets.ImageFolder(root=data, transform=transform)
+    
+    data_loader = torch.utils.data.DataLoader(data, batch_size,shuffle=True)
+    
+    return data_loader
+
+def main(args):
+    os.environ["SM_CHANNEL_TRAIN"]
+    os.environ["SM_CHANNEL_VALID"]
+    os.environ["SM_CHANNEL_TEST"]
+    
+    '''
+    TODO: Initialize a model by calling the net function
+    '''
+    model=net()
+    
+    '''
+    TODO: Create your loss and optimizer
+    '''
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.fc.parameters(), lr=0.001)
+    
+    
+    '''
+    TODO: Call the train function to start training your model
+    Remember that you will need to set up a way to get training data from S3
+    '''
+    model=train(model, train_loader, loss_criterion, optimizer)
+    
+    '''
+    TODO: Test the model to see its accuracy
+    '''
+    test(model, test_loader, criterion)
+    
+    '''
+    TODO: Save the trained model
+    '''
+    torch.save(model, path)
+
+if __name__=='__main__':
+    parser=argparse.ArgumentParser(description ="Train script" )
+    
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        default=64,
+        metavar="N",
+        help="input batch size for training (default: 64)",
+    )
+    parser.add_argument(
+        "--test-batch-size",
+        type=int,
+        default=1000,
+        metavar="N",
+        help="input batch size for testing (default: 1000)",
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=22
+        metavar="N",
+        help="number of epochs to train (default: 22)",
+    )
+    parser.add_argument(
+        "--lr", type=float, default=1.0, metavar="LR", help="learning rate (default: 1.0)"
+    )
+    args = parser.parse_args()
+
+    
+    
+    '''
+    TODO: Specify any training args that you might need
+    '''
+    parser.add_argument(
+        "-- key1", ---
+    )
+    parser.add_argument(
+        "-- key2", ---
+    )
+    args=parser.parse_args()
+    
+    main(args)
